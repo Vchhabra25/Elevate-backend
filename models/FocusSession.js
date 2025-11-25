92662e7267370b44bd0834ad535b4f2b057eb845
Vanshika <vanshikachhabra25@gmail.com>
2025-11-26 00:12:08 +0530

    Converted db + FocusSession model to CommonJS

--- models/FocusSession.js
+++ models/FocusSession.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const focusSessionSchema = new mongoose.Schema(
   {
@@ -8,4 +8,4 @@ const focusSessionSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("FocusSession", focusSessionSchema);
+module.exports = mongoose.model("FocusSession", focusSessionSchema);
